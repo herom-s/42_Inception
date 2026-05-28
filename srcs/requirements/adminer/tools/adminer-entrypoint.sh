699408b2d7c53840f44178c3bba9a1ed64077077
herom-s <heromapp1@gmail.com>
2026-05-28 19:03:26 -0300

    fix: fixed adminer and ftp

--- srcs/requirements/adminer/tools/adminer-entrypoint.sh
+++ srcs/requirements/adminer/tools/adminer-entrypoint.sh
@@ -3,4 +3,7 @@ set -e
 
 envsubst '${ADMINER_PORT},${WP_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
+mkdir -p /var/www/html/adminer
+cp /var/www/adminer/index.php /var/www/html/adminer/index.php
+
 exec "$@"
